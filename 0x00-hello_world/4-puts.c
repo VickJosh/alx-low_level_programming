bb3887aeb87308ccc76776bf3e48a2e66ed14007
joshua <fikayojoshu6@gmail.com>
2023-08-22 09:17:35 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point of the program
+ * Return: if the code runs successfully, it returns a value of 0
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
