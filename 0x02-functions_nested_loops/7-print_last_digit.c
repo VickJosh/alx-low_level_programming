1b03739e3d6d7725431f1633a366eb115e17739b
joshua <fikayojoshu6@gmail.com>
2023-08-29 15:24:35 -0700

    task 7 and 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
- *
+ *print_last_digit - the function prints the last digit of the number
+ *@i: function parameter
+ *Return: program to return a value of the last digit of the number
+ */
+
+int print_last_digit(int i)
+
+{
+	int k = i % 10;
+
+		if (i < 0)
+			k = -k;
+	_putchar (k + '0');
+
+	return (k);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *jack_bauer - function to print hours of jack bauer
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	for (hour = 0; hour < 24; hour++)
+	{
+		int minute;
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
